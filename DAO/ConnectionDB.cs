b84877e73b674af83d705ad8e1012b63653a0478
FELIPE KENZO OZAKO <felipeozako@gmail.com>
2024-11-07 20:09:25 -0300

    .

---
+++ DAO/ConnectionDB.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+
+namespace CadastroPizza.DAO
+{
+    public class ConnectionDB
+    {
+        public static SqlConnection GetConnection()
+        {
+            string strCon = "Data Source=LOCALHOST; Database=Cadastro; user id=sa; password=123456";
+            SqlConnection conexao = new SqlConnection(strCon);
+            conexao.Open();
+            return conexao;
+        }
+
+    }
+}
